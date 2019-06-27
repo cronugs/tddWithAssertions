bd224670ed822aa6e928e8a9b0df89d01e638541
cronugs <cronugs@outlook.com>
2019-06-27 11:39:52 +0200

    TDD with even number of evens

--- testing_before.py
+++ testing_before.py
@@ -1,14 +1,28 @@
-from random import randint
-
-numbers = []
-
-for num in range(20):
-    numbers.append(randint(0, 50))
 
 def even_number_of_evens(numbers):
-    if len(numbers) == 0:
-        return False
+        evens = 0
 
-assert even_number_of_evens(numbers) == False, "No numbers"
+        for i in numbers:                
+                if i %2 == 0:
+                        evens+=1
 
-print(numbers)
+        if len(numbers) == 0:
+                return False
+
+        if evens == 0:
+                return False
+
+        elif evens %2 == 0:
+                return True
+
+        elif evens %2 != 0:
+               return False   
+
+assert even_number_of_evens([]) == False, "No numbers"
+assert even_number_of_evens([2, 4]) == True, "Two even numbers"
+assert even_number_of_evens([2, 3]) == False, "Two numbers, only one even"
+assert even_number_of_evens([2, 3, 9, 10, 13, 7, 8]) == False, "Odd number of numbers"
+assert even_number_of_evens([2, 3, 9, 10, 13, 7, 8, 5, 12]) == True, "Even Number of evens"
+assert even_number_of_evens([1, 3, 9]) == False, "No even numbers"
+
+print("All tests passed")
