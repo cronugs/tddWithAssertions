89616744aed845ce0b92404e4aa090e9ca5ae822
cronugs <cronugs@outlook.com>
2019-06-27 12:38:38 +0200

    further refactored to more 'pythonic' single line for and if statements

--- testing_before.py
+++ testing_before.py
@@ -1,7 +1,7 @@
 
 def is_even(num):
         return num % 2 == 0
-
+'''
 def even_number_of_evens(numbers):
         evens = 0
         for i in numbers:                
@@ -12,6 +12,13 @@ def even_number_of_evens(numbers):
                 return False
         else :
                 return is_even(evens)   
+'''
+
+# The code above can be further refactored into a more pythonic form
+def even_number_of_evens(numbers):
+
+        evens = sum([1 for n in numbers if is_even(n)])
+        return False if evens == 0 else is_even(evens)
 
 assert even_number_of_evens([]) == False, "No numbers"
 assert even_number_of_evens([2, 4]) == True, "Two even numbers"
